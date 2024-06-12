630f6c3e1d14f310b88d100e8a97f09e373c4998
Matias <matias32p@gmail.com>
2024-06-12 20:20:16 -0300

    Add duplicated implementation

--- workflow/duplicateTransform.py
+++ workflow/duplicateTransform.py
@@ -1,4 +1,10 @@
 from logger import Logger
 
 def duplicateTransform(df):
-    return df
+    #Remove exact duplicates, keep the first occurrence
+    duplicated_rows = df[df.duplicated(keep='first')].tolist()
+    df_without_diuplicated = df.drop_duplicates(keep='first')
+    if len(duplicated_rows):
+        Logger.logWarning("Las siguientes filas fueron eliminadas al ser duplicados exactos:", duplicated_rows)
+
+    return df_without_diuplicated
