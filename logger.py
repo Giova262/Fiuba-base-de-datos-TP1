dd79659e13752309c97c89397547d9e867eb871e
Josue Giovanni Valdivia Urrutia <jvaldiviau@fi.uba.ar>
2024-06-11 21:35:22 -0300

    add logger

--- logger.py
+++ logger.py
@@ -1,31 +1,28 @@
-
 from colorama import Fore, Style, init
+import logging
+
+# Basic configuration for the logger
+logging.basicConfig(
+    level=logging.DEBUG,  # Set the log level to DEBUG
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",  # Log format
+    handlers=[
+        logging.FileHandler("app.log"),  # Log to a file
+        logging.StreamHandler(),  # Also log to console
+    ],
+)
+
+logger = logging.getLogger(__name__)
 
 class Logger:
-    
+
     def logWarning(index, message):
-        print(
-            Fore.LIGHTBLUE_EX
-            + "Row:"
-            + str(index)
-            + message
-            + Style.RESET_ALL
-        )
+        logger.critical("Row:" + str(index) + message )
+        print(Fore.LIGHTBLUE_EX + "Row:" + str(index) + message + Style.RESET_ALL)
 
     def logError(index, message):
-        print(
-            Fore.RED
-            + "Row:"
-            + str(index)
-            + message
-            + Style.RESET_ALL
-        )
+        logger.error("Row:" + str(index) + message )
+        print(Fore.RED + "Row:" + str(index) + message + Style.RESET_ALL)
 
     def logSuccess(index, message):
-        print(
-            Fore.GREEN
-            + "Row:"
-            + str(index)
-            + message
-            + Style.RESET_ALL
-        )
+        logger.info("Row:" + str(index) + message )
+        print(Fore.GREEN + "Row:" + str(index) + message + Style.RESET_ALL)
