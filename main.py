cc825ee892775c89b3c3ba85337e95ccc0db1e75
Joaquin Velazquez <jvelazquez@fi.uba.ar>
2024-06-12 13:23:24 -0300

    Merge

--- main.py
+++ main.py
@@ -1,76 +1,46 @@
 import pandas as pd
 
+from workflow.nullTransform import nullTransform
+from workflow.typeTransform import typeTransform
+from workflow.consistencyTransform import consistencyTransform
+from workflow.outlierTransform import outlierTransform
+from workflow.duplicateTransform import duplicateTransform
 
 def extract():
-    Logger.loginfo(" Starting ETL")
-    return pd.read_csv(
-        "dataset.csv",
-        usecols=[
-            "start_date",
-            "end_date",
-            "created_on",
-            "latitud",
-            "longitud",
-            "place_l2",
-            "place_l3",
-            "operation",
-            "property_type",
-            "property_rooms",
-            "property_bedrooms",
-            "property_surface_total",
-            "property_surface_covered",
-            "property_price",
-            "property_currency",
-            "property_title",
-        ],
-    )
-
+    cols_to_keep = ["start_date",
+                    "end_date",
+                    "created_on",
+                    "latitud",
+                    "longitud",
+                    "place_l2",
+                    "place_l3",
+                    "operation",
+                    "property_type",
+                    "property_rooms",
+                    "property_bedrooms",
+                    "property_surface_total",
+                    "property_surface_covered",
+                    "property_price",
+                    "property_currency",
+                    "property_title"]
+    
+    return pd.read_csv("dataset.csv", usecols=cols_to_keep)
 
 def transform(df):
-    nullCheck = NullCheck()
-    typeCheck = TypeCheck()
-    outlierCheck = OutlierCheck()
-    duplicatedCheck = DuplicatedCheck()
-    consistencyCheck = ConsistencyCheck()
+    df = nullTransform(df)
+    df = typeTransform(df)
+    df = consistencyTransform(df)
+    df = outlierTransform(df)
+    df = duplicateTransform(df)
 
-    new_df = []
-    new_df_with_erros = []
-    for index, row in df.iterrows():
-        (rowTemp, keep) = nullCheck.check(index, row)
-        if not keep:
-            new_df_with_erros.append(rowTemp)
-            continue
-
-        (rowTemp, keep) = typeCheck.check(index, rowTemp)
-        if not keep:
-            new_df_with_erros.append(rowTemp)
-            continue
-
-        (rowTemp, keep) = consistencyCheck.check(index, rowTemp)
-        if not keep:
-            new_df_with_erros.append(rowTemp)
-            continue
-
-        (rowTemp, keep) = outlierCheck.check(index, rowTemp)
-        if not keep:
-            new_df_with_erros.append(rowTemp)
-            continue
-
-        (rowTemp, keep) = duplicatedCheck.check(index, rowTemp)
-        if not keep:
-            new_df_with_erros.append(rowTemp)
-            continue
-
-        new_df.append(rowTemp)
-
-    return new_df
-
+    return df
 
 def main():
-    df = extract()
-    df = df.head()
-    transform(df)
+    print("Starting..")
 
+    df = extract()
+    df = transform(df)
+    print("Final shape: ", df.shape)
 
 if __name__ == "__main__":
     main()
